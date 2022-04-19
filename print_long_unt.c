1ab9646f9cf0b023ad0fbf45dc4daac5c3ff8e67
MariamAbosede <mhizoyindamolazyno325@gmail.com>
2022-04-19 15:53:34 -0700

    print_long_unt.c

--- print_long_unt.c
+++ print_long_unt.c
@@ -4,7 +4,7 @@
  * @arguments: number to print
  * @buf: buffer pointer
  * @ibuf: index for buffer pointer
- * Return: number of chars printed.
+ * Return:the number of chars printed.
  */
 int prinlunt(va_list arguments, char *buf, unsigned int ibuf)
 {
